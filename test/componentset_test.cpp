9ef730192834678acbc22a626642efa921a13381
Fredrik Åhs <freahs@kth.se>
2017-05-20 19:28:57 +0000

    test for ComponentSet

--- test/componentset_test.cpp
+++ test/componentset_test.cpp
@@ -2,6 +2,7 @@
 
 #include "componentset.hpp"
 #include "componentmanager.hpp"
+#include "typeid.hpp"
 
 TEST_CASE( "Building", "[componentset]" ) {
     struct C1 : public tyra::Component {};
@@ -11,10 +12,41 @@ TEST_CASE( "Building", "[componentset]" ) {
     struct C5 : public tyra::Component {};
     struct C6 : public tyra::Component {};
 
-    tyra::ComponentSet s1 = tyra::ComponentSet::build<C1, C2, C3, C4>();
-    tyra::ComponentSet s2 = tyra::ComponentSet::build<C2, C3, C4>();
-    tyra::ComponentSet s3 = tyra::ComponentSet::build<C4, C5>();
-    tyra::ComponentSet s4 = tyra::ComponentSet::build<C6>();
+    tyra::ComponentSet s1 = tyra::ComponentSet()
+        .add(tyra::Type<tyra::Component>::id<C1>())
+        .add(tyra::Type<tyra::Component>::id<C2>())
+        .add(tyra::Type<tyra::Component>::id<C3>())
+        .add(tyra::Type<tyra::Component>::id<C4>());
+    tyra::ComponentSet s2 = tyra::ComponentSet()
+        .add(tyra::Type<tyra::Component>::id<C2>())
+        .add(tyra::Type<tyra::Component>::id<C3>())
+        .add(tyra::Type<tyra::Component>::id<C4>());
+    tyra::ComponentSet s3 = tyra::ComponentSet()
+        .add(tyra::Type<tyra::Component>::id<C4>())
+        .add(tyra::Type<tyra::Component>::id<C5>());
+    tyra::ComponentSet s4 = tyra::ComponentSet()
+        .add(tyra::Type<tyra::Component>::id<C6>());
+    tyra::ComponentSet s5;
+
+    SECTION ("compare") {
+        tyra::ComponentSet s6 = tyra::ComponentSet()
+            .add(tyra::Type<tyra::Component>::id<C2>())
+            .add(tyra::Type<tyra::Component>::id<C3>())
+            .add(tyra::Type<tyra::Component>::id<C4>());
+        tyra::ComponentSet s7;
+        REQUIRE(s1 != s2);
+        REQUIRE(s2 == s6);
+        REQUIRE(s2 != s5);
+        REQUIRE(s5 == s7);
+    }
+
+    SECTION ("empty()") {
+        REQUIRE(s1.empty() == false);
+        REQUIRE(s2.empty() == false);
+        REQUIRE(s3.empty() == false);
+        REQUIRE(s4.empty() == false);
+        REQUIRE(s5.empty() == true);
+    }
 
     SECTION( "contains_all" ) {
         REQUIRE(s1.contains_all(s2) == true);
