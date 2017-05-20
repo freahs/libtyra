933c0bbdad7a316030dab1ce01c2de8f70f84762
Fredrik Åhs <freahs@kth.se>
2017-05-20 19:25:21 +0000

    changed components to be added by typeid

--- inc/componentset.hpp
+++ inc/componentset.hpp
@@ -12,28 +12,39 @@ namespace tyra {
     struct Component;
 
     class ComponentSet {
-    private:
+    public:
         typedef std::bitset<MAX_COMPONENT_TYPES> container_type;
         container_type m_bits;
         ComponentSet(container_type set) : m_bits(set) {} 
 
-        template<typename T> static void build(container_type&); 
-        template<typename T1, typename T2, typename... Ts> static void build(container_type&);
+        //template<typename T> static void build(container_type&); 
+        //template<typename T1, typename T2, typename... Ts> static void build(container_type&);
 
     public:
+        ComponentSet() = default;
         const container_type& bits() const;
-        template<typename T> static ComponentSet build(); 
-        template<typename T1, typename T2, typename... Ts> static ComponentSet build();
+        //template<typename T> static ComponentSet build(); 
+        //template<typename T1, typename T2, typename... Ts> static ComponentSet build();
 
+        ComponentSet& add(const TypeId&);
+        ComponentSet& remove(const TypeId&);
+
+        bool contains(const TypeId&) const;
         bool contains_all(const ComponentSet& other) const;
         bool contains_any(const ComponentSet& other) const;
         bool contains_none(const ComponentSet& other) const;
+
+        bool empty() const;
+
+        bool operator==(const ComponentSet& other) const;
+        bool operator!=(const ComponentSet& other) const;
     };
 
     const ComponentSet::container_type& ComponentSet::bits() const {
         return m_bits;
     }
 
+    /*
     template<typename T>
         ComponentSet ComponentSet::build() {
             container_type set;
@@ -61,6 +72,24 @@ namespace tyra {
             build<T2, Ts...>(c);
         }
 
+    */
+    ComponentSet& ComponentSet::add(const TypeId& type_id) {
+        m_bits[type_id] = 1;
+        return *this;
+    }
+
+    ComponentSet& ComponentSet::remove(const TypeId& type_id) {
+        m_bits[type_id] = 0;
+        return *this;
+    }
+
+    bool ComponentSet::contains(const TypeId& type_id) const {
+        if (static_cast<size_t>(type_id) >= m_bits.size()) {
+            return false;
+        }
+        return m_bits[type_id];
+    }
+
     bool ComponentSet::contains_all(const ComponentSet& other) const {
         if(this->m_bits.any() && (this->m_bits & other.m_bits) != other.m_bits) {
             return false;
@@ -80,6 +109,18 @@ namespace tyra {
             return false;
         }
         return true;
+    }
+
+    bool ComponentSet::empty() const {
+        return m_bits.none();
+    }
+
+    bool ComponentSet::operator==(const ComponentSet& other) const {
+        return m_bits == other.m_bits;
+    }
+
+    bool ComponentSet::operator!=(const ComponentSet& other) const {
+        return !operator==(other);
     }
 }
 
