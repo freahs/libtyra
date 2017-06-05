0b385aa7015a2e083386f6b61ed91d1a108ff605
Fredrik Åhs <freahs@users.noreply.github.com>
2017-06-05 07:17:34 +0200

    Update system.cpp

--- src/system.cpp
+++ src/system.cpp
@@ -39,36 +39,6 @@ namespace tyra {
             m_entities.erase(id);
             entityRemoved(id);
         }
-
-        /*
-           bool has_all = false;
-           bool has_one = false;
-           bool has_excluded = false;
-
-           if (m_require_all_components.count() == 0 && m_require_one_components.count() == 0) {
-           return;
-           }
-
-           for (size_t i = 0; i < MAX_COMPONENT_TYPES; ++i) {
-           if (m_exclude_components[i] && entity_components[i]) { has_excluded = true; break; }
-           if (m_require_all_components[i] && !entity_components[i]) { has_all = false; break; }
-           if (m_require_one_components[i] && entity_components[i]) has_one = true;
-           if (m_require_all_components[i] && entity_components[i]) has_all = true;
-           }
-
-           bool is_present = m_entities.find(id) != m_entities.end();
-
-           if (is_present){
-           if (has_excluded) removeEntity(id);
-           else if (!has_one && !has_all) removeEntity(id);
-           else if (!has_one && m_require_one_components.any()) removeEntity(id);
-           else if (!has_all && m_require_all_components.any()) removeEntity(id);
-           } else if(!has_excluded && !is_present) {
-           if (has_all && has_one) addEntity(id);
-           else if (has_one && !has_all && m_require_all_components.none()) addEntity(id);
-           else if (has_all && !has_one && m_require_one_components.none()) addEntity(id);
-           }
-           */
     }
 
     void System::update() {
